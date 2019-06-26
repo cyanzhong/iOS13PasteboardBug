ecdbdadbc35d3e8077e34f239e7110e87e196876
Ying Zhong <0x00eeee@gmail.com>
2019-06-26 18:17:03 +0800

    Update ViewController.swift

--- PasteboardDemo/ViewController.swift
+++ PasteboardDemo/ViewController.swift
@@ -19,24 +19,24 @@ class ViewController: UIViewController {
     textView.textContainerInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
     textView.text =
     """
-    # Before repro:
+    # Before repro
 
     - Make sure you're using iOS 13 Beta 2
     - Copy some text to pasteboard
 
-    In Shortcuts.app:
+    # In Shortcuts.app
 
     - Create a Shortcut
     - Search "PasteboardDemo"
     - Add it as an action
 
-    In Xcode:
+    # In Xcode
 
     - Attach "IntentExtension" as the debugging target
     - Choose Shortcuts.app to run
     - Run the Shorcut we have just created
 
-    Result:
+    # Result
 
     - UIPasteboard.general.string is always nil
     - UIPasteboard.general.items is an empty array
